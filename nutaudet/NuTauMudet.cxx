25cfe73ea97246a6e68888077bc6fe1004b8d6e0
Evgenii Kurbatov <Genek@Mac-mini-Evgenij.local>
2019-06-06 02:03:30 +0300

    new size

--- nutaudet/NuTauMudet.cxx
+++ nutaudet/NuTauMudet.cxx
@@ -453,7 +453,7 @@ void NuTauMudet::ConstructGeometry()
       Double_t mLength = 950. * cm;
       Double_t x_tr = 450. * cm;
       Double_t y_tr = 700. * cm;
-      Double_t leftZPosition = - 3420 * cm;
+      Double_t leftZPosition = - 3425 * cm;
       Double_t rightZPosition = leftZPosition + 2. * microGap + cubeThickness + mLength;
       Double_t zMid = (leftZPosition+rightZPosition)/2.;
 
