f62d931f5d2b4eb96c2cd766609c80e436f99861
Evgenii Kurbatov <lelituser@gmail.com>
2021-11-08 14:42:37 +0300

    newd

--- passive/ShipMuonShield.cxx
+++ passive/ShipMuonShield.cxx
@@ -575,8 +575,7 @@ Int_t ShipMuonShield::Initialize(std::vector<TString> &magnetName,
     auto f = TFile::Open(fGeofile, "read");
     TVectorT<Double_t> params;
     params.Read("params");
-    // for (auto i = params.begin(); i != params.end(); ++i)
-    //   std::cout << *i << ' ';
+
     const int offset = 7;
 
     dXIn[0] = 0.4 * m;
