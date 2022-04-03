2d7afa4fb50557bde9beb55b7d812ba3e0fff385
Evgeniy Kurbatov <lelituser@gmail.com>
2022-04-04 00:08:58 +0300

    floor fix

--- passive/ShipMuonShield.cxx
+++ passive/ShipMuonShield.cxx
@@ -39,7 +39,7 @@ ShipMuonShield::ShipMuonShield(TString geofile,
   auto f = TFile::Open(geofile, "read");
   TVectorT<Double_t> params;
   params.Read("params");
-  Double_t LE = 10. * m, floor = 5. * m;
+  Double_t LE = 10. * m, floor = 4.2 * m;
   fDesign = 8;
   fField = 1.7;
   dZ0 = 1 * m;
@@ -967,8 +967,8 @@ void ShipMuonShield::ConstructGeometry()
     double  cover_vert_position = ((max_y + cover_thickness/2. + 10.*cm) + cover_down_position)/2.;
     TGeoVolume *cover_left_box = gGeoManager->MakeBox("left_cover", concrete, cover_thickness/2., cover_vert_height/2., cover_length/2.);
     TGeoVolume *cover_right_box = gGeoManager->MakeBox("right_cover", concrete, cover_thickness/2., cover_vert_height/2., cover_length/2.);
-    tShield->AddNode(cover_left_box, 1, new TGeoTranslation(-(cover_hor_width/2. + cover_thickness/2. + 1.*cm), cover_vert_position, cover_z_position));
-    tShield->AddNode(cover_right_box, 1, new TGeoTranslation((cover_hor_width/2. + cover_thickness/2. + 1.*cm), cover_vert_position, cover_z_position));
+    tShield->AddNode(cover_left_box, 1, new TGeoTranslation(-(cover_hor_width/2. + cover_thickness/2. + 1.*cm), -cover_vert_position, cover_z_position));
+    tShield->AddNode(cover_right_box, 1, new TGeoTranslation((cover_hor_width/2. + cover_thickness/2. + 1.*cm), -cover_vert_position, cover_z_position));
 
   }
       Double_t dX1 = dXIn[0];
