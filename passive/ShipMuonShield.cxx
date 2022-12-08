9af89fd15cde0d3f66fa4bf4558011f9670c49e9
Evgeniy Kurbatov <lelituser@gmail.com>
2022-12-08 13:07:11 +0300

    box instead of arb8

--- passive/ShipMuonShield.cxx
+++ passive/ShipMuonShield.cxx
@@ -893,17 +893,20 @@ void ShipMuonShield::ConstructGeometry()
 		     HmainSideMagOut[nM], gapIn[nM], gapOut[nM], Z[nM], nM==8, fStepGeo);
 //Add some kind of protection here
   if(nM==8){
-    TGeoUniformMagField *magFieldIron_s_p = new TGeoUniformMagField(0., 0., 0.);
-    TGeoUniformMagField *RetField_s_p     = new TGeoUniformMagField(0., 0., 0.);
-    TGeoUniformMagField *ConRField_s_p    = new TGeoUniformMagField(0., 0., 0.);
-    TGeoUniformMagField *ConLField_s_p    = new TGeoUniformMagField(0., 0., 0.);
-    TGeoUniformMagField *fields_s_p[4] = {magFieldIron_s_p,RetField_s_p,ConRField_s_p,ConLField_s_p};
-    Double_t protection_width = 8. * m;
-    Double_t protection_height = 8. * m;
-    CreateMagnet("Protection", iron, tShield, fields_s_p, fieldDirection[nM],
-         protection_width/4., (protection_height/2.-protection_width/4.), protection_width/4., (protection_height/2.-protection_width/4.), 0.1*m,
-         midGapIn[nM], midGapOut[nM], HmainSideMagIn[nM],
-         HmainSideMagOut[nM], 1.*mm, 1.*mm, Z[nM]+dZf[nM]/2.+200.*mm, 0, 0);
+    TGeoVolume *snd_protection = gGeoManager->MakeBox("SND_Protection", iron, 5.*m, 5.*m-5.*cm, 5.*cm);
+    tShield->AddNode(snd_protection, 1, new TGeoTranslation(0., 0., Z[nM]+dZf[nM]/2.+200.*mm));
+
+    // TGeoUniformMagField *magFieldIron_s_p = new TGeoUniformMagField(0., 0., 0.);
+    // TGeoUniformMagField *RetField_s_p     = new TGeoUniformMagField(0., 0., 0.);
+    // TGeoUniformMagField *ConRField_s_p    = new TGeoUniformMagField(0., 0., 0.);
+    // TGeoUniformMagField *ConLField_s_p    = new TGeoUniformMagField(0., 0., 0.);
+    // TGeoUniformMagField *fields_s_p[4] = {magFieldIron_s_p,RetField_s_p,ConRField_s_p,ConLField_s_p};
+    // Double_t protection_width = 8. * m;
+    // Double_t protection_height = 10. * m - 1. * cm;
+    // CreateMagnet("Protection", iron, tShield, fields_s_p, fieldDirection[nM],
+    //      protection_width/4., (protection_height/2.-protection_width/4.), protection_width/4., (protection_height/2.-protection_width/4.), 0.1*m,
+    //      midGapIn[nM], midGapOut[nM], HmainSideMagIn[nM],
+    //      HmainSideMagOut[nM], 1.*mm, 1.*mm, Z[nM]+dZf[nM]/2.+200.*mm, 0, 0);
   }
 
 	if (nM==8 || !fSupport) continue;
